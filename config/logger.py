bee42e69986cd482949f847d70f988ef7339a700
zhangjunjie <2251285929@qq.com>
2024-10-16 19:51:43 +0800

    1016 修改了日志配置

--- config/logger.py
+++ config/logger.py
@@ -3,8 +3,12 @@ import logging
 def setup_logger(name, log_file):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(log_file, mode='a')
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    
+    # 检查是否已经有handler避免重复添加
+    if not logger.hasHandlers():
+        handler = logging.FileHandler(log_file, mode='a')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
     return logger
